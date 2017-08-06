e4d1b8090ccbe8bdbfe0ca0c06359b7994afd2c0
orvalho <rasa396@gmail.com>
2017-08-07 00:04:41 +0300

    Add files via upload

---
+++ 8kyu/find_maximum_and_minimum_values_of_a_list.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Make two functions (maximum and minimum) that outputs, respectively,
+the largest and the lowest number in array.
+*/
+
+function maximum($array) {
+  return max($array);
+}
+
+function minimum($array) {
+  return min($array);
+}
