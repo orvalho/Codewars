d0ccef4abf8fc65341e74f758b4619576c4eb8c7
orvalho <rasa396@gmail.com>
2017-08-10 23:25:56 +0300

    Add files via upload

---
+++ 8kyu/basic_variable_assignment.php
@@ -0,0 +1,13 @@
+<?php
+/*
+This code should store "codewa.rs" as a variable called "name" but it's not working.
+Fix it.
+
+$a == "code";
+$b == "wa.rs";
+$name == $a + $b;
+*/
+
+$a = "code";
+$b = "wa.rs";
+$name = $a . $b;
