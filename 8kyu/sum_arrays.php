8b58dfb732818dee092c8147ba24ecfb8fd774be
orvalho <rasa396@gmail.com>
2017-08-30 21:56:12 +0300

    Add files via upload

---
+++ 8kyu/sum_arrays.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Write a function that takes an array of numbers
+and returns the sum of them.
+If the array does not contain any numbers
+return 0.
+*/
+
+function sum(array $a): float {
+  return array_sum($a);
+}
