e916016d13e936a29751a6bd9d540cadbcde2da5
orvalho <rasa396@gmail.com>
2017-09-05 20:16:21 +0300

    Add files via upload

---
+++ 8kyu/parse_nice_int_from_char_problem.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Write a function to return integer (0-9)
+from given string ("1 year old" or "5 years old", etc.).
+The first char in given string is integer.
+*/
+function getAge($response) {
+  return $response{0};
+}
